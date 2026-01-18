fd79941dc8f37d2d83ff6bccbf8925f32577bc04
anly <1284558387@qq.com>
2026-01-18 21:55:36 +0100

    understanding calling conventions

---
+++ nasm/callmaxofthree.c
@@ -0,0 +1,20 @@
+/**
+ * nasm -fmacho64 maxofthree.asm
+ * cc maxofthree.o callmaxofthree.c
+ * ./a.out
+ */ 
+
+ #include <stdio.h>
+#include <inttypes.h>
+
+int64_t maxofthree(int64_t, int64_t, int64_t);
+
+int main() {
+    printf("%ld\n", maxofthree(1, -4, -7));
+    printf("%ld\n", maxofthree(2, -6, 1));
+    printf("%ld\n", maxofthree(2, 3, 1));
+    printf("%ld\n", maxofthree(-2, 4, 3));
+    printf("%ld\n", maxofthree(2, -6, 5));
+    printf("%ld\n", maxofthree(2, 4, 6));
+    return 0;
+}
